97d844ebfebcbb621c1e73af416b3e155b9ea004
abdelaaziz khouda <abdelkhouda055@gmail.com>
2025-05-29 00:38:46 +0100

    designing app

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to scribeHub app to share stories and chat with community
+    </div>
+  );
+}
+
+export default App;
