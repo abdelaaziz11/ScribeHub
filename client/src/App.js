99ed34fad6f1d707c27988645cf66ab2a43e022f
abdelaaziz khouda <abdelkhouda055@gmail.com>
2025-06-23 13:27:05 +0100

    make home page

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Home from './Components/Home/Home'
 function App() {
   return (
     <div className="App">
-      Welcome to scribeHub app to share stories and chat with community
       <Navbar/>
       <Home/>
     </div>
