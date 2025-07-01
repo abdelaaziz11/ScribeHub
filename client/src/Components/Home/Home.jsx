64384fc92e7bc943af8c556aeee91f84cd6b1324
abdelaaziz khouda <abdelkhouda055@gmail.com>
2025-07-01 23:24:58 +0100

    build and edit my home page

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -5,6 +5,14 @@ const Home = () => {
   return (
     <>
     <div className="home">
+      <section className='home_hero'>
+        <div className="left_hero">
+          <h1>Welcome</h1>
+          <p>This is a Community Chat and Story Sharing</p>
+        </div>
+        <div className="right_hero">
+        </div>
+      </section>
     </div>
     </>
   )
