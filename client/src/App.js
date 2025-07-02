5c92163a57ce42a70d7fe821bdd00feb3cc1d846
abdelaaziz khouda <abdelkhouda055@gmail.com>
2025-07-02 13:41:15 +0100

    build my home page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <Footer/>
     </div>
   );
 }
